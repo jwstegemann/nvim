6d2f841d910099f86108a3c864b461ea20be37e2
Jens Stegemann <jwstegemann@gmail.com>
2023-05-22 16:22:01 +0200

    cleanup

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -29,8 +29,10 @@ vim.keymap.set('n', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 
 -- Searches
 vim.keymap.set('n', '<leader>sc', require('telescope.builtin').commands, { desc = '[S]earch [C]ommands' })
-vim.keymap.set('n', '<leader>tt', require('toggleterm').toggle_command, { desc = '[T]oggle [T]erminal' })
 vim.keymap.set('n', '<leader>so', require('telescope.builtin').buffers, { desc = '[S]earch [O]pen Buffers' })
+
+-- Terminal
+vim.keymap.set('n', '<leader>tt', require('toggleterm').toggle_command, { desc = '[T]oggle [T]erminal' })
 
 -- Trouble
 vim.keymap.set("n", "<leader>tx", "<cmd>TroubleToggle<cr>", { desc = '[T]oggle [X] Trouble' })
@@ -42,6 +44,8 @@ vim.keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>", { desc = '[T
 vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>", { desc = '[T]oggle [Q]uickfix' })
 -- vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { desc = '[T]oggle [X] Trouble'})
 
+-- LSP
 vim.keymap.set("n", "<leader>cf", "<cmd>Format<cr>", { desc = '[C]ode [F]ormat' })
 
+-- File-Tree
 vim.keymap.set("n", "<leader>tf", "<cmd>NvimTreeToggle<cr>", { desc = '[T]oggle [F]ile Tree' })
