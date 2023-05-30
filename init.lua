f2c2d6223f1aa91f1754ad02c38a566abcfc7c28
Jens Stegemann <jwstegemann@gmail.com>
2023-05-30 11:31:31 +0200

    added autowrite

--- init.lua
+++ init.lua
@@ -532,3 +532,6 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.opt.autowriteall = true
+vim.opt.autoread = true
